c35a7e242d03f2aafdf2ac57cb9355cad35fbd18
Kevin Adoboe <kevin.adoboe@circleci.com>
2025-10-29 11:38:06 -0700

    Add test case to validate basic arithmetic
    This test intentionally has a bug where 2+2 is expected to equal 5, which should cause the CI to fail.

--- op-service/util_test.go
+++ op-service/util_test.go
@@ -86,3 +86,11 @@ func TestParse256BitChainID(t *testing.T) {
 		})
 	}
 }
+
+// TestMathValidation checks basic arithmetic - intentionally wrong to test CI
+func TestMathValidation(t *testing.T) {
+	result := 2 + 2
+	expected := 5 // BUG: This is intentionally wrong to trigger CI failure
+
+	require.Equal(t, expected, result, "Basic math should work")
+}
